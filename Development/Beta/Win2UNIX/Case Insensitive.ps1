f8d4fa10fed46f68377e5933ef0bcfb239bb22f0
Effi Ovadia <Effi.Ovadia@GMail.com>
2023-05-17 23:07:42 +0800

    deleted:    Development/Beta/Add Users (Admins).ps1 	deleted:    Development/Beta/Add Users (Standard).ps1 	deleted:    Development/Beta/Console Theme.ps1 	deleted:    Development/Beta/VirtualBox.ps1 	deleted:    Development/Beta/Win2UNIX/Case Insensitive.ps1 	deleted:    Development/DNS over HTTPS.ps1 	deleted:    Development/DriveMapper.ps1 	deleted:    Development/Languages.ps1 	deleted:    Development/vscode/settings.json 	deleted:    README.md 	deleted:    Windows Server/Add DNS Forwarder.txt 	deleted:    Windows Server/Bluetooth/Add Bluetooth.txt 	deleted:    Windows Server/Bluetooth/driver/hidbth.inf 	deleted:    Windows Server/Bluetooth/driver/hidbth.sys deleted:    Windows Server/Convert to Workstation.ps1 	deleted:    Windows Server/Languages.ps1 	deleted:    Windows Server/OneDrive.ps1 	deleted:    Windows Server/disable DEP.cmd 	deleted:    Windows Server/performance.ps1 	deleted:    applications.ps1 	deleted:    create users/Create-Users.ps1 deleted:    create users/passwd.administrator.txt 	deleted:    create users/passwd.effio.txt 	deleted:    registry_tweaks.ps1 	deleted:    ssh_&_rdp.ps1

--- Development/Beta/Win2UNIX/Case Insensitive.ps1
+++
@@ -1,2 +0,0 @@
-Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Kernel" obcaseinsensitive -Force -Value 0
-Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" DontPrettyPath -Force -Value 1
