bb1ad4a3c86304161314cdaa79c7dcf67f565eeb
Effi Ovadia <Effi.Ovadia@GMail.com>
2023-06-08 05:57:04 +0800

    new file:   EnvironmentVariable.ps1 	renamed:    Language UI/UI+Chinese.ps1 -> UI Languages/Chinese.ps1 	renamed:    Language UI/UI+English.ps1 -> UI Languages/English.ps1 	renamed:    Language UI/UI+Hebrew.ps1 -> UI Languages/Hebrew.ps1 	renamed:    openssh-server.ps1 -> openssh.ps1

---
+++ EnvironmentVariable.ps1
@@ -0,0 +1,10 @@
+$DATA = "XYZ"
+
+#@ Set Variable "VAR" for current user
+[System.Environment]::SetEnvironmentVariable('VAR',$DATA,[System.EnvironmentVariableTarget]::User)
+
+#@ Set Variable "VAR" for all users
+If ($Admin)
+  {
+  [System.Environment]::SetEnvironmentVariable('VAR',$DATA,[System.EnvironmentVariableTarget]::Machine)
+  }
