9a9ec83e721c6b8cc0e94d0823ec3d1bb5960b5a
Effi Ovadia <Effi.Ovadia@GMail.com>
2024-12-23 04:49:21 +0200

    modified:   HowTo/Yubikey.ps1

--- HowTo/Yubikey.ps1
+++ HowTo/Yubikey.ps1
@@ -3,20 +3,18 @@ cd "C:\Program Files\Yubico\YubiKey Manager"
 .\ykman.exe fido credentials list
 # .\ykman.exe fido credentials delete [credential_id]
 
-ssh-keygen -t ed25519-sk -O resident -C "991 Yubikey 5C" -f "$ENV:USERPROFILE/.ssh/id_ed25519_sk_(15-013-991)"
-ssh-keygen -p -f "$ENV:USERPROFILE/.ssh/id_ed25519_sk_(15-013-991)"
+ssh-keygen -a 100 -t ed25519 -f "$ENV:USERPROFILE/.ssh/id_ed25519_bitbucket" -C "bitbucket"
 
-ssh-keygen -t ed25519-sk -O resident -C "545 Yubikey 5 nfc" -f "$ENV:USERPROFILE/.ssh/id_ed25519_sk_(18-069-545)"
-ssh-keygen -p -f "$ENV:USERPROFILE/.ssh/id_ed25519_sk_(18-069-545)"
-
-ssh-keygen -t ed25519-sk -O resident -C "822 Yubikey 5 nfc" -f "$ENV:USERPROFILE/.ssh/id_ed25519_sk_(16-670-822)"
-ssh-keygen -p -f "$ENV:USERPROFILE/.ssh/id_ed25519_sk_(16-670-822)"
-
-ssh-keygen -t ed25519-sk -O resident -C "275 Yubikey 5C nano (XPS)" -f "$ENV:USERPROFILE/.ssh/id_ed25519_sk_(16-397-275)"
-ssh-keygen -p -f "$ENV:USERPROFILE/.ssh/id_ed25519_sk_(16-397-275)"
-
-ssh-keygen -t ed25519-sk -O resident -C "(189) Yubikey 5 nano (NUC12)" -f "$ENV:USERPROFILE/.ssh/id_ed25519_sk_(13-135-189)"
-ssh-keygen -p -f "$ENV:USERPROFILE/.ssh/id_ed25519_sk_(13-135-189)"
+ssh-keygen -a 100 -t ed25519-sk -O resident -O application=ssh:991 -f "$ENV:USERPROFILE/.ssh/id_ed25519_sk_(15-013-991)" -C "991 Yubikey 5C"
+#ssh-keygen -p -f "$ENV:USERPROFILE/.ssh/id_ed25519_sk_(15-013-991)"
+ssh-keygen -a 100 -t ed25519-sk -O resident -O application=ssh:545 -f "$ENV:USERPROFILE/.ssh/id_ed25519_sk_(18-069-545)" -C "545 Yubikey 5 nfc"
+#ssh-keygen -p -f "$ENV:USERPROFILE/.ssh/id_ed25519_sk_(18-069-545)"
+ssh-keygen -a 100 -t ed25519-sk -O resident -O application=ssh:822 -f "$ENV:USERPROFILE/.ssh/id_ed25519_sk_(16-670-822)" -C "822 Yubikey 5 nfc"
+#ssh-keygen -p -f "$ENV:USERPROFILE/.ssh/id_ed25519_sk_(16-670-822)"
+ssh-keygen -a 100 -t ed25519-sk -O resident -O application=ssh:275 -f "$ENV:USERPROFILE/.ssh/id_ed25519_sk_(16-397-275)" -C "275 Yubikey 5C nano (XPS)" 
+#ssh-keygen -p -f "$ENV:USERPROFILE/.ssh/id_ed25519_sk_(16-397-275)"
+ssh-keygen -a 100 -t ed25519-sk -O resident -O application=ssh:189 -f "$ENV:USERPROFILE/.ssh/id_ed25519_sk_(13-135-189)" -C "189 Yubikey 5 nano (NUC12)" 
+#ssh-keygen -p -f "$ENV:USERPROFILE/.ssh/id_ed25519_sk_(13-135-189)"
 
 .\ykman.exe fido credentials list
 .\ykman openpgp info
